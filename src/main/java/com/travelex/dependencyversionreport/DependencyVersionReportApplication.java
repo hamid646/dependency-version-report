c9bb48f83a4bd697c8be4163f2d795474853ea57
hamid646 <hamid.mortazavi@travelex.com>
2017-11-08 16:54:35 +0000

    first commit

---
+++ src/main/java/com/travelex/dependencyversionreport/DependencyVersionReportApplication.java
@@ -0,0 +1,12 @@
+package com.travelex.dependencyversionreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyVersionReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyVersionReportApplication.class, args);
+	}
+}
